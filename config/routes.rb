a18d006b0fbdf7b03d64f6d85d33109eca739e9c
qiaoyu <qiaoyu1111358@gmail.com>
2017-10-09 11:29:41 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'groups#index'
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
 end
